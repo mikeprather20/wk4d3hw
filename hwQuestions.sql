f81b1dfdbc298b4e99fc698e7e91bc4ef97691e8
Michael Prather <mikeprather20@gmail.com>
2022-07-21 09:42:08 -0500

    start

---
+++ hwQuestions.sql
@@ -0,0 +1,39 @@
+--1. List all customers who live in Texas (use JOINs).
+--ANSWER =
+
+
+
+--2. Get all payments above $6.99 with the Customer's Full Name.
+--ANSWER =
+
+
+
+--3. Show all customers names who have made payments over $175(use subqueries).
+--ANSWER =
+
+
+
+--4. List all customers that live in Nepal (use the city table).
+--ANSWER =
+
+
+
+--5. Which staff member had the most transactions?
+--ANSWER =
+
+
+
+--6. How many movies of each rating are there?
+--ANSWER =
+
+
+
+--7.Show all customers who have made a single payment above $6.99 (Use Subqueries).
+--ANSWER =
+
+
+
+--8. How many free rentals did our stores give away?
+--ANSWER =
+
+
